006f2a350588a62449c29f018c341b67e5f6fa58
Sandeep Pradhan <sandeeppradhan@Sandeeps-MacBook-Air-2.local>
2025-08-19 00:16:03 +0530

    add another function to the library for the Biclass_BDTG training

--- TMVA_BDT.C
+++ TMVA_BDT.C
@@ -17,24 +17,9 @@ void TMVA_BDT(int nclasses = 2) {
     // --- Init TMVA
     TMVA::Tools::Instance();
     // --- Input signal and background
-    TFile *fsig = TFile::Open(sig_file);
-    TFile *fbkg = TFile::Open(bkg_file);
-    TTree *tsig_raw = (TTree*)fsig->Get(tree_name);
-    TTree *tbkg_raw = (TTree*)fbkg->Get(tree_name);
-
-    // Clean: remove NaN/Inf in T_alphaqq
-    TString cleanCut = "!isnan(T_alphaqq) && !isinf(T_alphaqq)";
-    TTree *tsig = tsig_raw->CopyTree(cleanCut);
-    TTree *tbkg = tbkg_raw->CopyTree(cleanCut);
-    // --- Cuts for training/testing split
-    TCut cut_even("T_event % 2 == 0");
-    TCut cut_odd ("T_event % 2 == 1");
-
-    // --- Train even/odd models
-    TrainModel(tsig, tbkg, cut_even,result_even_path, "Even", weights_even_path,class_even_path);
-    TrainModel(tsig, tbkg, cut_odd,result_odd_path ,"Odd", weights_odd_path,class_odd_path);
-
-    std::cout << "==> Training finished, weights saved as  " <<weights_even_path << " and "<<  weights_odd_path << std::endl;
+    if (nclasses == 2) {
+        TrainBiclassBDTG();
+    }
     predict_bdt_score(pred_file);
 
 };
--- tmva_library.h
+++ tmva_library.h
@@ -102,3 +102,25 @@ auto TrainModel = [](TTree *sig, TTree *bkg, TCut cut, TString out_result_path ,
 
     };
 
+auto TrainBiclassBDTG = [](){
+    TFile *fsig = TFile::Open(sig_file);
+    TFile *fbkg = TFile::Open(bkg_file);
+    TTree *tsig_raw = (TTree*)fsig->Get(tree_name);
+    TTree *tbkg_raw = (TTree*)fbkg->Get(tree_name);
+
+    // Clean: remove NaN/Inf in T_alphaqq
+    TString cleanCut = "!isnan(T_alphaqq) && !isinf(T_alphaqq)";
+    TTree *tsig = tsig_raw->CopyTree(cleanCut);
+    TTree *tbkg = tbkg_raw->CopyTree(cleanCut);
+    // --- Cuts for training/testing split
+    TCut cut_even("T_event % 2 == 0");
+    TCut cut_odd ("T_event % 2 == 1");
+
+    // --- Train even/odd models
+    TrainModel(tsig, tbkg, cut_even,result_even_path, "Even", weights_even_path,class_even_path);
+    TrainModel(tsig, tbkg, cut_odd,result_odd_path ,"Odd", weights_odd_path,class_odd_path);
+
+    std::cout << "==> Training finished, weights saved as  " <<weights_even_path << " and "<<  weights_odd_path << std::endl;
+
+};
+
